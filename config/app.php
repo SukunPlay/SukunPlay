573487776c05185c2f282c0301c1e1c64852a563
saidey <saidey@outlook.com>
2020-02-26 15:52:32 +0500

    controllers

--- config/app.php
+++ config/app.php
@@ -166,6 +166,8 @@ return [
          * Package Service Providers...
          */
 
+        Scottybo\LaravelFacebookSdk\LaravelFacebookSdkServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -225,6 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Facebook' => Scottybo\LaravelFacebookSdk\FacebookFacade::class,
 
     ],
 
