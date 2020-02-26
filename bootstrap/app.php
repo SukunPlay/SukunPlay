7e43e614ae3e976950d652384135649e798e9949
saidey <saidey@outlook.com>
2020-02-26 15:38:58 +0500

    some

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,8 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+$app->register(Scottybo\LaravelFacebookSdk\LaravelFacebookSdkServiceProvider::class);
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
