fc8502fdd6535fbbea023a8111a5c583f534a4ae
unleasheds <unleasheds@gmail.com>
2020-03-14 16:26:47 +0500

    new updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
     <!-- Custom fonts for this template-->
     <link href="{{asset('dashboard/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
     <!-- Custom styles for this template-->
     <link href="{{ asset('dashboard/css/sukun.min.css') }}" rel="stylesheet">
     <!-- Bootstrap core JavaScript-->
@@ -33,18 +34,19 @@
 
     @yield('content')
 
+    @include('particles.footer')
 
 </div>
 <!-- End of Content Wrapper -->
 
 </div>
 <!-- End of Page Wrapper -->
-@include('particles.footer')
 
 <!-- Scroll to Top Button-->
 <a class="scroll-to-top rounded" href="#page-top">
     <i class="fas fa-angle-up"></i>
 </a>
+@include('particles.logout')
 
 
 
