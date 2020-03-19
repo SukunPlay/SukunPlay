8904fba32f4dd8a33fd5c325af46dd0b24832be9
saidey <saidey@outlook.com>
2020-03-19 23:18:51 +0500

    sds

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -166,27 +166,6 @@
 
     <body>
 
-{{--    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>--}}
-{{--    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>--}}
-
-{{--    <div class="holder">--}}
-{{--        <iframe width="560" height="315" src="https://www.youtube.com/embed/Q4FK_syYYtQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>--}}
-{{--        <div class="overlay trigger" src="https://www.youtube.com/embed/Q4FK_syYYtQ" data-target="#videoModal" data-toggle="modal"></div>--}}
-{{--    </div>--}}
-
-
-
-{{--    <div class="modal fade" id="videoModal" tabindex="-1" role="dialog" aria-labelledby="videoModal" aria-hidden="true">--}}
-{{--        <div class="modal-dialog modal-lg" role="document">--}}
-{{--            <h5 class="modal-title"></h5>--}}
-{{--            <div class="modal-content">--}}
-{{--                <div class="embed-responsive embed-responsive-16by9">--}}
-{{--                    <iframe class="embed-responsive-item" src="" allowfullscreen></iframe>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-
 
     <div class="container">
         <div class="row">
