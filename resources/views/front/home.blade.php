3c24b5581a07b9b43c78f3645a960915733bc96e
saidey <saidey@outlook.com>
2020-03-25 03:05:46 +0500

    koo

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -262,7 +262,7 @@
 
                 </section>
                 <section class="bg-light ">
-                    <div class="container p-4 col-12">
+                    <div class="container p-4 p-lg-15 col-12">
                         <div class="row" id="post_data">
 
 
