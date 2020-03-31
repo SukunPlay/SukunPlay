44e8941cc63738b2638df0af68b4992191696db0
saidey <saidey@outlook.com>
2020-03-31 20:56:47 +0500

    some

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,15 +8,19 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function home() {
+    public function home()
+    {
         return view('dashboard.home');
     }
-    public function index() {
+
+    public function index()
+    {
         return view('home');
     }
 
 
-    public function test3(){
+    public function test3()
+    {
 
         $item = StoreVideo::all()->sortByDesc('fb_created');
 
@@ -35,14 +39,96 @@ class DashboardController extends Controller
         $video_id = $request->video_id;
 
         //find video
-        $video = StoreVideo::where('fb_id','=',$video_id)->first();
+        $video = StoreVideo::where('fb_id', '=', $video_id)->first();
 
 
-        $carbon = Carbon::make($video->fb_created);
+//        $carbon = Carbon::make($video->fb_created);
 
-        $video->diffz = $carbon->diffForHumans();
+        $date = \Carbon\Carbon::make($video->fb_created);
+        $month = $date->month;
+        $dmonth = null;
 
-        if($video){
+        switch ($month) {
+
+            case 1;
+
+                $dmonth = "ޖަނަވަރީ";
+
+                break;
+
+            case 2;
+
+                $dmonth = "ފެބުރުވަރީ";
+
+                break;
+
+            case 3;
+
+                $dmonth = "މާރިޗު";
+
+                break;
+
+            case 4;
+
+                $dmonth = "އެޕްރީލު";
+
+                break;
+
+            case 5;
+
+                $dmonth = "މޭ";
+
+                break;
+
+            case 6;
+
+                $dmonth = "ޖޫން";
+
+                break;
+
+            case 7;
+
+                $dmonth = "ޖުލައި";
+
+                break;
+
+            case 8;
+
+                $dmonth = "އޮގަސްޓް";
+
+                break;
+
+            case 9;
+
+                $dmonth = "ސެޕްޓެންބަރު";
+
+                break;
+
+            case 10;
+
+                $dmonth = "އޮކްޓޫބަރު";
+
+                break;
+
+            case 11;
+
+                $dmonth = "ނޮވެންބަރު";
+
+                break;
+
+            case 12;
+
+                $dmonth = "ޑިސެންބަރު";
+
+                break;
+
+
+        }
+
+
+        $video->diffz = $date->year."&#x202b;".$date->day."&nbsp;".$dmonth."&nbsp;";
+
+        if ($video) {
             return $video->toArray();
         }
     }
--- resources/views/front/test/home3.blade.php
+++ resources/views/front/test/home3.blade.php
@@ -273,9 +273,93 @@
                                                     </div>
                                                 </div>
                                             </div>
+                                            @php
+                                                $date = \Carbon\Carbon::make($return['sample']->fb_created);
+                                                $month = $date->month;
+                                                $dmonth= null;
+
+                                                switch ($month){
+
+                                                    case 1;
+
+                                                    $dmonth= "ޖަނަވަރީ";
+
+                                                    break;
+
+                                                    case 2;
+
+                                                    $dmonth= "ފެބުރުވަރީ";
+
+                                                    break;
+
+                                                     case 3;
+
+                                                    $dmonth= "މާރިޗު";
+
+                                                    break;
+
+                                                     case 4;
+
+                                                    $dmonth= "އެޕްރީލު";
+
+                                                    break;
+
+                                                     case 5;
+
+                                                    $dmonth= "މޭ";
+
+                                                    break;
+
+                                                     case 6;
+
+                                                    $dmonth= "ޖޫން";
+
+                                                    break;
+
+                                                     case 7;
+
+                                                    $dmonth= "ޖުލައި";
+
+                                                    break;
+
+                                                     case 8;
+
+                                                    $dmonth= "އޮގަސްޓް";
+
+                                                    break;
+
+                                                     case 9;
+
+                                                    $dmonth= "ސެޕްޓެންބަރު";
+
+                                                    break;
+
+                                                     case 10;
+
+                                                    $dmonth= "އޮކްޓޫބަރު";
+
+                                                    break;
+
+                                                     case 11;
+
+                                                    $dmonth= "ނޮވެންބަރު";
+
+                                                    break;
+
+                                                     case 12;
+
+                                                    $dmonth= "ޑިސެންބަރު";
+
+                                                    break;
+
+
+                                                }
+
+
+                                            @endphp
                                             <div class="col-7 pr-0 ">
                                                 <p style="font-family: MVAWAHEED; font-size: 15px;text-align: right ;">
-                                                    2020&#x202b;03&nbsp;މާރޗް&nbsp;</p>
+                                                    {{$date->year}}&#x202b;{{$date->day}}&nbsp;{{$dmonth}}&nbsp;</p>
                                             </div>
                                         </div>
 
@@ -487,8 +571,8 @@
                                                         </div>
                                                     </div>
                                                     <div class="col-7 pr-0">
-                                                        <p style="font-family: MVAWAHEED; font-size: 15px; float: right; direction: rtl">
-                                                            2020&#x202b;03&nbsp;މާރޗް&nbsp;</p>
+                                                        <p style="font-family: MVAWAHEED; font-size: 15px; float: right; direction: rtl" id="video_created_at">
+                                                            </p>
                                                     </div>
                                                 </div>
 
@@ -773,7 +857,7 @@
 
                     $('#video_title').html(result.title);
                     $('#video_desc').html(result.desc);
-                    $('#video_created_at').html("last updated " + result.diffz)
+                    $('#video_created_at').html(result.diffz)
 
                 }
             });
