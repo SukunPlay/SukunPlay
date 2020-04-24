2e577857e7cd73674afc161bb82f6d76b9d772bb
saidey <saidey@outlook.com>
2020-04-24 16:05:09 +0500

    back

--- public/js/app.js
+++ public/js/app.js
@@ -49935,8 +49935,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/SukunPlay/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /var/www/SukunPlay/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Applications/XAMPP/xamppfiles/htdocs/SukunPlay/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Applications/XAMPP/xamppfiles/htdocs/SukunPlay/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
