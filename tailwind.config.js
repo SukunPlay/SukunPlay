99bb789aef55320ca73c1f69ff99e56909b6254a
saidey <saidey@outlook.com>
2020-04-24 16:02:24 +0500

    sads

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 module.exports = {
     theme: {
-        colors: {
-            sbtnprm: {
-                light:'#8CD790',
-                dark:'#00A651',
+        extend: {
+            colors: {
+                sbtnprm: {
+                    light: '#8CD790',
+                    dark: '#00A651',
+                },
             },
         },
-        extend: {},
     },
     variants: {},
     plugins: [
