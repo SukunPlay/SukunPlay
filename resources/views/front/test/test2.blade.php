94abcff6ff9f5ec1570f442e3c37b2c811a87de5
saidey <saidey@outlook.com>
2020-04-25 17:53:10 +0500

    gg

--- resources/views/front/test/test2.blade.php
+++ resources/views/front/test/test2.blade.php
@@ -24,23 +24,86 @@
 </head>
 <body>
 <div class="hidden xl:block">
-    <section class="w-auto bg-sukun-official">
+    <section class="bg-sukun-official">
         <div class="container mx-auto px-10">
-            <div class="flex justify-between p-4">
-                <input class="border-solid border w-1/5 h-8 rounded-full text-white" placeholder="" style="direction: rtl;font-family:MVAWAHEED;background: url('/search2.svg') no-repeat scroll 5px 5px;padding: 10px;">
+            <div class="flex justify-between pb-4 pt-4">
+                <div class="flex rounded-full border-solid border hover:bg-sukungreen-200">
+                    <div class="p-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="21.555" height="21.555"
+                             viewBox="0 0 21.555 21.555">
+                            <g transform="translate(12.63 7.999)">
+                                <path d="M300.421,190a.421.421,0,1,0,.421.421A.421.421,0,0,0,300.421,190Z"
+                                      transform="translate(-300 -190)"
+                                      fill="#FFFFFF"/>
+                            </g>
+                            <path
+                                d="M21.061,18.68l-5.619-5.619a8.344,8.344,0,0,0,1.4-4.641,8.42,8.42,0,1,0-8.42,8.42,8.345,8.345,0,0,0,4.641-1.4L14.614,17h0l4.065,4.065a1.684,1.684,0,1,0,2.381-2.381Zm-8.2-4.125h0a7.585,7.585,0,1,1,1.692-1.692A7.614,7.614,0,0,1,12.863,14.555Zm.883.382a8.448,8.448,0,0,0,1.19-1.19l1.194,1.194a10.219,10.219,0,0,1-1.19,1.19Zm6.72,5.529a.841.841,0,0,1-1.19,0l-3.738-3.738a11.062,11.062,0,0,0,1.19-1.19l3.738,3.738a.841.841,0,0,1,0,1.19Z"
+                                fill="#FFFFFF"/>
+                            <g transform="translate(1.684 1.684)">
+                                <path
+                                    d="M46.736,40a6.736,6.736,0,1,0,6.736,6.736A6.743,6.743,0,0,0,46.736,40Zm0,12.63a5.894,5.894,0,1,1,5.894-5.894A5.9,5.9,0,0,1,46.736,52.63Z"
+                                    transform="translate(-40 -40)" fill="#FFFFFF"/>
+                            </g>
+                            <g transform="translate(7.999 3.368)">
+                                <path
+                                    d="M195.139,83.245A5.06,5.06,0,0,0,190.421,80a.421.421,0,1,0,0,.842,4.239,4.239,0,0,1,3.932,2.7.421.421,0,1,0,.786-.3Z"
+                                    transform="translate(-190 -80)" fill="#FFFFFF"/>
+                            </g>
+                        </svg>
+                    </div>
+                    <input placeholder="" type="text"
+                           class="rounded-full rounded-l-none bg-sukun-official text-white hover:bg-sukungreen-200 focus:outline-none focus:bg-sukungreen-200"
+                           style="direction: rtl; padding-right: 10px">
+                </div>
                 <img src="{{asset('logo.svg')}}" class="">
             </div>
         </div>
     </section>
 
+    <section>
+      <div class="container mx-auto px-10">
+          <div class="flex justify-between bg-gray-100 mt-10 pt-2 pb-2 pr-8 pl-8 text-sukungreen-400 items-center" style="direction: rtl; font-family: MVAWAHEED">
+              <div class="w-1/4 text-2xl" style="font-family: AAMUFKF">
+                  މިއަދުގެ ނަމާދު ވަގުތު
+              </div>
+              <div class="flex">
+                  ފަތިސް
+                  <div class="bg-white align-middle mr-2 rounded-sm" style="font-family: '.SF NS'">
+                      <p class="pr-2 pl-2">19:34</p>
+                  </div>
+              </div>
+              <div class="flex">
+                  މެންދުރު
+                  <div class="bg-white align-middle mr-2 rounded-sm" style="font-family: '.SF NS'">
+                      <p class="pr-2 pl-2">19:34</p>
+                  </div>
+              </div>
+              <div class="flex">
+                  އަސުރު
+                  <div class="bg-white align-middle mr-2 rounded-sm" style="font-family: '.SF NS'">
+                      <p class="pr-2 pl-2">19:34</p>
+                  </div>
+              </div>
+              <div class="flex">
+                  މަޢްރިބު
+                  <div class="bg-white align-middle mr-2 rounded-sm" style="font-family: '.SF NS'">
+                      <p class="pr-2 pl-2">19:34</p>
+                  </div>
+              </div>
+              <div class="flex">
+                  އިޝާ
+                  <div class="bg-white align-middle mr-2 rounded-sm" style="font-family: '.SF NS'">
+                      <p class="pr-2 pl-2">19:34</p>
+                  </div>
+              </div>
 
+
+          </div>
+      </div>
+    </section>
 
 
 </div>
-
-
-
-
 
 </body>
 <footer>
